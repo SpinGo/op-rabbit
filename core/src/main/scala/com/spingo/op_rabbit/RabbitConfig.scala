d6244229dd964976d9205896f98cd4ad366f6c20
Tim Harper <timcharper@gmail.com>
2015-09-02 12:37:04 -0600

    remove failed attempt at backwards compatibility

--- core/src/main/scala/com/spingo/op_rabbit/RabbitConfig.scala
+++ core/src/main/scala/com/spingo/op_rabbit/RabbitConfig.scala
@@ -5,19 +5,9 @@ import com.typesafe.config.ConfigFactory
 
 object RabbitConfig {
   // TODO - when removing legacy config support, remember to update reference.conf.
-  lazy val connectionConfig = {
-    val c = ConfigFactory.load()
-    Try { c.getConfig("op-rabbit.connection") } getOrElse {
-      System.err.println("WARNING! Connection configuration 'op-rabbit.connection' is not defined; falling back to 'rabbitmq'. See the README for the new configuration format; this backwards compatibility will be removed, eventually")
-      c.getConfig("rabbitmq")
-    }
-  }
+  lazy val connectionConfig =
+    ConfigFactory.load().getConfig("op-rabbit.connection")
 
-  lazy val systemConfig = {
-    val c = ConfigFactory.load()
-    Try { c.getConfig("op-rabbit") } getOrElse {
-      System.err.println("WARNING! System configuration 'op-rabbit' is not defined; falling back to 'rabbitmq'. See the README for the new configuration format; this backwards compatibility will be removed, eventually")
-      c.getConfig("rabbitmq")
-    }
-  }
+  lazy val systemConfig =
+    ConfigFactory.load().getConfig("op-rabbit")
 }
