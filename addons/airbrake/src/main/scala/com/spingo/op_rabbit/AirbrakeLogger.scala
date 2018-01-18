SpinGo/op-rabbit

commit b051d8c4db2cd2d1880b5596bb313afb6fc87402

Fix most compiler warnings

addons/airbrake/src/main/scala/com/spingo/op_rabbit/AirbrakeLogger.scala
@@ -5,7 +5,7 @@ import com.rabbitmq.client.Envelope
 import com.rabbitmq.client.AMQP.BasicProperties
 import org.slf4j.LoggerFactory
 import com.typesafe.config.ConfigFactory
-import scala.collection.JavaConversions._
+import scala.collection.convert.ImplicitConversions._
 
 /**
   == BATTERIES NOT INCLUDED ==
@@ -36,10 +36,11 @@ import scala.collection.JavaConversions._
 
   */
 class AirbrakeLogger(appName: String, airbrakeKey: String, environment: String) extends RabbitErrorLogging {
-  val log = LoggerFactory.getLogger(getClass.getName)
+  private val log = LoggerFactory.getLogger(getClass.getName)
+
   def apply(name: String, message: String, exception: Throwable, consumerTag: String, envelope: Envelope, properties: BasicProperties, body: Array[Byte]): Unit = try {
     val notice = new AirbrakeNoticeBuilder(airbrakeKey, exception, environment) {
-      setRequest(s"consumer://$appName/${name}", "consume") // it's a faux URL, but reduces weirdness when clicking in airbrake webapp
+      setRequest(s"consumer://$appName/$name", "consume") // it's a faux URL, but reduces weirdness when clicking in airbrake webapp
 
       val headerProperties: Map[String, String] = Option(properties.getHeaders) map { _.map { case (k,v) => (s"HEADER:$k", v.toString) }.toMap } getOrElse Map.empty
 

core/src/main/scala/com/spingo/op_rabbit/ConnectionParams.scala
@@ -8,7 +8,7 @@ import com.rabbitmq.client.SaslConfig
 import com.rabbitmq.client.impl.DefaultExceptionHandler
 import com.typesafe.config.Config
 import javax.net.SocketFactory
-import scala.collection.JavaConversions.{mapAsJavaMap, seqAsJavaList}
+import scala.collection.JavaConverters._
 import scala.util.Try
 
 /** Because topology recovery strategy configuration is crucial to how op-rabbit works, we don't allow some options to
@@ -37,7 +37,7 @@ case class ConnectionParams(
   saslConfig: SaslConfig = DefaultSaslConfig.PLAIN,
   sharedExecutor: Option[java.util.concurrent.ExecutorService] = None,
   shutdownTimeout: Int = ConnectionFactory.DEFAULT_SHUTDOWN_TIMEOUT,
-  socketFactory: SocketFactory = SocketFactory.getDefault()
+  socketFactory: SocketFactory = SocketFactory.getDefault
 ) {
   // TODO - eliminate ClusterConnectionFactory after switching to use RabbitMQ's topology recovery features.
   protected [op_rabbit] def applyTo(factory: ClusterConnectionFactory): Unit = {
@@ -53,7 +53,7 @@ case class ConnectionParams(
     factory.setRequestedFrameMax(requestedFrameMax)
     factory.setRequestedHeartbeat(requestedHeartbeat)
     factory.setSaslConfig(saslConfig)
-    sharedExecutor foreach (factory.setSharedExecutor)
+    sharedExecutor foreach factory.setSharedExecutor
     factory.setShutdownTimeout(shutdownTimeout)
     factory.setSocketFactory(socketFactory)
     if (ssl) factory.useSslProtocol()
@@ -61,22 +61,20 @@ case class ConnectionParams(
 }
 
 object ConnectionParams {
-  private def readHosts(config: Config): java.util.List[String] = {
-    Try(config.getStringList("hosts")).getOrElse(readCommaSeparatedHosts(config))
+  private def readHosts(config: Config): Seq[String] = {
+    Try(config.getStringList("hosts").asScala).getOrElse(readCommaSeparatedHosts(config))
   }
 
-  private def readCommaSeparatedHosts(config: Config): java.util.List[String] =
-    seqAsJavaList(
-      config
-        .getString("hosts")
-        .split(",")
-        .map(_.trim)
-    )
+  private def readCommaSeparatedHosts(config: Config): Seq[String] =
+    config
+      .getString("hosts")
+      .split(",")
+      .map(_.trim)
 
   def fromConfig(config: Config = RabbitConfig.connectionConfig) = {
-    val connectionFactory = new ClusterConnectionFactory()
-    val hosts = readHosts(config).toArray(new Array[String](0))
+    val hosts = readHosts(config)
     val port = config.getInt("port")
+
     ConnectionParams(
       hosts = hosts.map { h => new Address(h, port) },
       connectionTimeout = config.getDuration("connection-timeout", java.util.concurrent.TimeUnit.MILLISECONDS).toInt,

core/src/main/scala/com/spingo/op_rabbit/properties/HeaderValue.scala
@@ -3,7 +3,8 @@ package com.spingo.op_rabbit.properties
 import java.nio.charset.Charset
 import java.util.Date
 import com.rabbitmq.client.LongString
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import scala.collection.convert.ImplicitConversionsToScala._
 
 /**
   Trait which represents all values allowed in property generc headers
@@ -13,10 +14,8 @@ import scala.collection.JavaConversions._
 sealed trait HeaderValue {
   def value: Any
   def serializable: Object
-  def asString(sourceCharset: Charset): String =
-    value.toString
-  def asString: String =
-    asString(Charset.defaultCharset)
+  def asString(sourceCharset: Charset): String = value.toString
+  def asString: String = asString(Charset.defaultCharset)
 
   def asOpt[T](implicit conversion: FromHeaderValue[T]): Option[T] = conversion(this).right.toOption
 }
@@ -24,7 +23,7 @@ object HeaderValue {
   case class LongStringHeaderValue(value: LongString) extends HeaderValue {
     def serializable = value
     override def asString(sourceCharset: Charset) =
-      new String(value.getBytes(), sourceCharset)
+      new String(value.getBytes, sourceCharset)
   }
   case class StringHeaderValue(value: String) extends HeaderValue {
     def serializable = value
@@ -37,18 +36,18 @@ object HeaderValue {
   case class BigDecimalHeaderValue(value: BigDecimal) extends HeaderValue {
     val serializable = value.bigDecimal
     if (serializable.unscaledValue.bitLength() > 32)
-      throw new IllegalArgumentException("BigDecimal too large to be encoded");
+      throw new IllegalArgumentException("BigDecimal too large to be encoded")
   }
   case class DateHeaderValue(value: Date) extends HeaderValue {
     val serializable = value
   }
   case class MapHeaderValue(value: Map[String, HeaderValue]) extends HeaderValue {
-    lazy val serializable = mapAsJavaMap(value.mapValues(_.serializable))
+    lazy val serializable = value.mapValues(_.serializable).asJava
     override def asString(sourceCharset: Charset) = {
       val b = new StringBuilder()
       b += '{'
       for { (key, value) <- value } {
-        b ++= s"${key} = ${value.asString(sourceCharset)}"
+        b ++= s"$key = ${value.asString(sourceCharset)}"
         b += ','
       }
       b.deleteCharAt(b.length - 1)

addons/akka-stream/src/main/scala/com/spingo/op_rabbit/stream/MessagePublisherSink.scala
@@ -4,7 +4,7 @@ package stream
 import akka.actor.{ActorRef,Props}
 import akka.actor.FSM
 import akka.pattern.ask
-import akka.stream.scaladsl.{Flow, Keep, Sink}
+import akka.stream.scaladsl.Sink
 import akka.stream.actor._
 import scala.concurrent.{Future, Promise}
 import scala.concurrent.duration._
@@ -22,9 +22,10 @@ private class MessagePublisherSinkActor(rabbitControl: ActorRef, timeoutAfter: F
   import ActorSubscriberMessage._
   import MessagePublisherSinkActor._
 
+  private val queue = scala.collection.mutable.Map.empty[Long, Promise[Unit]]
+  private val completed = Promise[Unit]
+
   startWith(Running, ())
-  var queue = scala.collection.mutable.Map.empty[Long, Promise[Unit]]
-  val completed = Promise[Unit]
 
   override val requestStrategy = new MaxInFlightRequestStrategy(max = qos) {
     override def inFlightInternally: Int = queue.size
@@ -75,12 +76,12 @@ private class MessagePublisherSinkActor(rabbitControl: ActorRef, timeoutAfter: F
 
   onTransition {
     case Running -> Stopping if queue.isEmpty  =>
-      stopWith(Success())
+      stopWith(Success(()))
   }
 
   onTermination {
     case e: StopEvent =>
-      stopWith(Success())
+      stopWith(Success(()))
   }
 
   private val handleResponse: Message.ConfirmResponse => Unit = {

core/src/main/scala/com/spingo/op_rabbit/RabbitControl.scala
@@ -126,7 +126,7 @@ class RabbitControl(connection: Either[ConnectionParams, ActorRef]) extends Acto
       confirmedPublisher.tell(m, sender)
 
     case m: MessageForPublicationLike =>
-      publishChannel ! ChannelMessage(m.apply(_), dropIfNoChannel = m.dropIfNoChannel)
+      publishChannel ! ChannelMessage(m.apply, dropIfNoChannel = m.dropIfNoChannel)
 
     case GetConnectionActor =>
       sender ! connectionActor
@@ -140,9 +140,15 @@ class RabbitControl(connection: Either[ConnectionParams, ActorRef]) extends Acto
     case q: Subscription =>
       val initializedP = Promise[Unit]
       val closedP = Promise[Unit]
-      val subscriptionActorRef = context.actorOf(
-        Props(new SubscriptionActor(q, connectionActor, initializedP, closedP)),
-        name = s"subscription-${java.net.URLEncoder.encode(q.queue.queueName)}-${sequence.next}")
+      val subscriptionActorRef = {
+        val queueNameEncoded = java.net.URLEncoder.encode(q.queue.queueName, java.nio.charset.StandardCharsets.UTF_8.name)
+        val name = s"subscription-$queueNameEncoded-${sequence.next()}"
+
+        context.actorOf(
+          Props(new SubscriptionActor(q, connectionActor, initializedP, closedP)),
+          name = name
+        )
+      }
 
       context watch subscriptionActorRef
       // TODO - we need this actor to know the currect subscription state
@@ -153,6 +159,6 @@ class RabbitControl(connection: Either[ConnectionParams, ActorRef]) extends Acto
 
     case c: SubscriptionCommand =>
       running = c
-      subscriptions map (_ ! c)
+      subscriptions foreach (_ ! c)
   }
 }

core/src/main/scala/com/spingo/op_rabbit/impl/AsyncAckingRabbitConsumer.scala
@@ -2,14 +2,16 @@ package com.spingo.op_rabbit
 package impl
 
 import java.util.concurrent.atomic.AtomicInteger
+
 import akka.actor.{Actor, ActorLogging, Terminated}
 import akka.pattern.pipe
 import com.rabbitmq.client.AMQP.BasicProperties
 import com.spingo.op_rabbit.RabbitExceptionMatchers.{ConnectionGoneException, NonFatalRabbitException}
 import com.newmotion.akka.rabbitmq.{Channel, DefaultConsumer, Envelope}
 import scala.collection.mutable
-import scala.concurrent.{ExecutionContext, Future, Promise, Await}
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import scala.concurrent.duration._
+import scala.util.Failure
 
 private [op_rabbit] object ConsumerId {
   val count = new AtomicInteger()
@@ -52,9 +54,12 @@ private [op_rabbit] class AsyncAckingRabbitConsumer[T](
       ()
   }
 
-  def async(handler: Handler)(implicit ec: ExecutionContext): Handler = { (p, delivery) =>
-    Future { handler(p, delivery) } onFailure { case ex => p.failure(ex) }
-  }
+  def async(handler: Handler)(implicit ec: ExecutionContext): Handler =
+    (p, delivery) =>
+      Future(handler(p, delivery)).onComplete {
+        case Failure(t) => p.failure(t)
+        case _ =>
+      }
 
   def connected(channel: Channel, consumerTag: Option[String]): Receive = {
     case Subscription.SetQos(qos) =>
@@ -103,7 +108,7 @@ private [op_rabbit] class AsyncAckingRabbitConsumer[T](
       handleAckOrNack(r, channel)
       if (pendingDeliveries.isEmpty)
         context stop self
-    case Delivery(consumerTag, envelope, properties, body) =>
+    case Delivery(_, envelope, _, _) =>
       // note! Before RabbitMQ 2.7.0 does not preserve message order when this happens!
       // https://www.rabbitmq.com/semantics.html
       channel.basicReject(envelope.getDeliveryTag, true)
@@ -137,7 +142,7 @@ private [op_rabbit] class AsyncAckingRabbitConsumer[T](
 
   def handleUnsubscribe(channel: Channel, consumerTag: Option[String]): Unit = {
     try {
-      consumerTag.foreach(channel.basicCancel(_))
+      consumerTag.foreach(channel.basicCancel)
     } catch {
       case NonFatalRabbitException(_) =>
         ()
@@ -151,11 +156,11 @@ private [op_rabbit] class AsyncAckingRabbitConsumer[T](
       /* if deliveryTag not in pendingDeliveries, this means we've already
        * restarted the actor due to some unhandled exception, the channel was
        * closed and therefore this deliveryTag invalid */
-      return ()
+      return
     }
 
     rejectOrAck match {
-      case ack: ReceiveResult.Ack =>
+      case _: ReceiveResult.Ack =>
         pendingDeliveries.remove(deliveryTag)
         channel.basicAck(deliveryTag, false)
       case nack: ReceiveResult.Nack =>
@@ -184,24 +189,24 @@ private [op_rabbit] class AsyncAckingRabbitConsumer[T](
             log.error(e,
               "Some kind of connection issue likely caused our recovery strategy to fail; Nacking with requeue = true.")
 
-            ReceiveResult.Nack(deliveryTag, true)
+            ReceiveResult.Nack(deliveryTag, requeue = true)
           case ReceiveResult.Fail(_, _, exception) =>
             log.error(exception,
               "Recovery strategy failed, or something else went horribly wrong; " +
                 "Nacking with requeue = true, then shutting consumer down.")
             self ! Shutdown(Some(exception))
-            ReceiveResult.Nack(deliveryTag, true)
+            ReceiveResult.Nack(deliveryTag, requeue = true)
         }
         handleAckOrNack(nextStep, channel)
     }
   } catch {
-    case NonFatalRabbitException(e) =>
+    case NonFatalRabbitException(_) =>
       // Ignore
       ()
   }
 
   /**
-    * Applies the providen handler; unhandled exceptions are caught and recovered as a Fail AckOfNack type.
+    * Applies the provided handler; unhandled exceptions are caught and recovered as a Fail AckOfNack type.
     */
   private def applyHandler(whileText: String, delivery: Delivery)(fn: Handler): Future[ReceiveResult] = {
     val handled = Promise[ReceiveResult]
@@ -209,10 +214,10 @@ private [op_rabbit] class AsyncAckingRabbitConsumer[T](
     try fn(handled, delivery)
     catch {
       case e: Throwable =>
-        handled.trySuccess(ReceiveResult.Fail(delivery, Some(s"Error while ${whileText}"), e))
+        handled.trySuccess(ReceiveResult.Fail(delivery, Some(s"Error while $whileText"), e))
     }
     handled.future.recover { case e =>
-      ReceiveResult.Fail(delivery, Some(s"Unhandled exception occurred while ${whileText}"), e)
+      ReceiveResult.Fail(delivery, Some(s"Unhandled exception occurred while $whileText"), e)
     }(context.dispatcher)
   }
 }

core/src/main/scala/com/spingo/op_rabbit/properties/HeaderValueConverter.scala
@@ -1,9 +1,6 @@
 package com.spingo.op_rabbit.properties
 
-import com.rabbitmq.client.LongString
 import java.nio.charset.Charset
-import java.util.Date
-import scala.collection.JavaConversions._
 
 /**
   Trait which describes converters that convert a [[HeaderValue]] from one type to another.
@@ -107,7 +104,7 @@ object FromHeaderValue {
       case IntHeaderValue(v)        => Right(BigDecimal(v))
       case ByteHeaderValue(v)       => Right(BigDecimal(v))
       case DoubleHeaderValue(v)     => Right(BigDecimal(v))
-      case FloatHeaderValue(v)      => Right(BigDecimal(v))
+      case FloatHeaderValue(v)      => Right(BigDecimal(v.toDouble))
       case LongHeaderValue(v)       => Right(BigDecimal(v))
       case ShortHeaderValue(v)      => Right(BigDecimal(v))
       case BigDecimalHeaderValue(v) => Right(v)

commit b386dbe6a90d3ea709f66b33f9eea32bbb217f71

Fix scala 2.11 compilation issues introduced in b051d8c

addons/airbrake/src/main/scala/com/spingo/op_rabbit/AirbrakeLogger.scala
@@ -5,7 +5,7 @@ import com.rabbitmq.client.Envelope
 import com.rabbitmq.client.AMQP.BasicProperties
 import org.slf4j.LoggerFactory
 import com.typesafe.config.ConfigFactory
-import scala.collection.convert.ImplicitConversions._
+import scala.collection.JavaConversions._
 
 /**
   == BATTERIES NOT INCLUDED ==

build.sbt
@@ -16,8 +16,8 @@ val assertNoApplicationConf = taskKey[Unit]("Makes sure application.conf isn't p
 val commonSettings = Seq(
   organization := "com.spingo",
   version := appProperties.getProperty("version"),
-  scalaVersion := "2.12.4",
-  crossScalaVersions := Seq("2.11.8", "2.12.4"),
+  scalaVersion := "2.11.12",
+  crossScalaVersions := Seq("2.11.12", "2.12.4"),
   libraryDependencies ++= Seq(
     "com.chuusai" %%  "shapeless" % "2.3.3",
     "com.typesafe" % "config" % "1.3.2",

core/src/main/scala/com/spingo/op_rabbit/ConnectionParams.scala
@@ -46,7 +46,7 @@ case class ConnectionParams(
     factory.setPassword(password)
     factory.setVirtualHost(virtualHost)
     // Replace the table of client properties that will be sent to the server during subsequent connection startups.
-    factory.setClientProperties(mapAsJavaMap(clientProperties))
+    factory.setClientProperties(clientProperties.asJava)
     factory.setConnectionTimeout(connectionTimeout)
     factory.setExceptionHandler(exceptionHandler)
     factory.setRequestedChannelMax(requestedChannelMax)

core/src/main/scala/com/spingo/op_rabbit/properties/HeaderValue.scala
@@ -4,7 +4,6 @@ import java.nio.charset.Charset
 import java.util.Date
 import com.rabbitmq.client.LongString
 import scala.collection.JavaConverters._
-import scala.collection.convert.ImplicitConversionsToScala._
 
 /**
   Trait which represents all values allowed in property generc headers
@@ -122,7 +121,7 @@ object HeaderValue {
   implicit val convertFromByteArray      : ToHeaderValue[Array[Byte]          , ByteArrayHeaderValue]  = ByteArrayHeaderValue(_)
   implicit def convertFromMap[T](implicit converter: ToHeaderValue[T, HeaderValue]): ToHeaderValue[Map[String, T], MapHeaderValue]         = { m => MapHeaderValue(m.mapValues(converter)) }
   implicit def convertFromSeq[T](implicit converter: ToHeaderValue[T, HeaderValue]): ToHeaderValue[Seq[T], SeqHeaderValue]                 = { s => SeqHeaderValue(s.map(converter)) }
-  implicit def convertFromJavaList[T](implicit converter: ToHeaderValue[T, HeaderValue]): ToHeaderValue[java.util.List[T], SeqHeaderValue] = { list => SeqHeaderValue(list.map(converter)) }
+  implicit def convertFromJavaList[T](implicit converter: ToHeaderValue[T, HeaderValue]): ToHeaderValue[java.util.List[T], SeqHeaderValue] = { list => SeqHeaderValue(list.asScala.map(converter)) }
 
   def apply[T](value: T)(implicit converter: ToHeaderValue[T, HeaderValue]): HeaderValue =
     if (value == null) NullHeaderValue else converter(value)
@@ -134,7 +133,7 @@ object HeaderValue {
     case v: java.math.BigDecimal                 => apply(v)
     case v: java.util.Date                       => apply(v)
     case v: java.util.Map[_, _] =>
-      MapHeaderValue(v.map {
+      MapHeaderValue(v.asScala.map {
         case (k, v: Object) => (k.toString, from(v))
         case (k, otherwise) =>
           throw new RuntimeException(
@@ -149,7 +148,7 @@ object HeaderValue {
     case v: Array[Byte]                          => apply(v)
     case null                                    => NullHeaderValue
     case v: java.util.List[_] =>
-      SeqHeaderValue(v.map { case v: Object => from(v) })
+      SeqHeaderValue(v.asScala.map { case v: Object => from(v) })
     case v: Array[Object] =>
       SeqHeaderValue(v.map(from))
     case otherwise =>